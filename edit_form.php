4fb6082e49eb94a602182467cc4bdfd7c2cde05c
Frederic Nevers <fredericnevers@gmail.com>
2013-06-18 20:45:47 +0800

    Allow for user defined title

--- edit_form.php
+++ edit_form.php
@@ -29,7 +29,12 @@ class block_broken_links_edit_form extends block_edit_form {
     protected function specific_definition($mform) {
 
         // Section header title according to language file.
-        $mform->addElement('header', 'configheader', get_string('blocksettings', 'block'));        
+        $mform->addElement('header', 'configheader', get_string('blocksettings', 'block'));
+        
+        // A sample string variable with a default value.
+		$mform->addElement('text', 'config_title', get_string('blocktitle', 'block_broken_links'));
+		$mform->setDefault('config_title', 'default value');
+		$mform->setType('config_title', PARAM_MULTILANG);        
 
     }
 }
